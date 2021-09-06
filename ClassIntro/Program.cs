c20b82d4d32914378e15a76d24665afcee463a1f
ttezc <ttezc@ARESING>
2021-09-06 19:54:49 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raid Raid1 = new Raid();
+            Raid1.KarakterAdi = "Aresing";
+            Raid1.Rol = "DPS";
+            Raid1.OynadıgıClass = "Priest";
+            Raid1.ItemLevel = 242;
+
+
+            Raid Raid2 = new Raid();
+            Raid2.KarakterAdi = "Jûr";
+            Raid2.Rol = "Healer";
+            Raid2.OynadıgıClass = "Priest";
+            Raid2.ItemLevel = 240;
+
+
+            Raid Raid3 = new Raid();
+            Raid3.KarakterAdi = "Ulsia";
+            Raid3.Rol = "Tank";
+            Raid3.OynadıgıClass = "Demon Hunter";
+            Raid3.ItemLevel = 236;
+
+            Raid[] RaidKadrosu = new Raid[] {Raid1, Raid2, Raid3 };
+
+            foreach (Raid raid in RaidKadrosu)
+            {
+                Console.WriteLine(raid.KarakterAdi + " " + raid.OynadıgıClass + " " + raid.Rol + " " + raid.ItemLevel);    
+            }
+
+        }
+    }
+
+    class Raid
+    {
+        public string KarakterAdi { get; set; }
+        public string Rol { get; set; }
+        public string OynadıgıClass { get; set; }
+        public int ItemLevel { get; set; }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "1";
+            string kurs2 = "2";
+            string kurs3 = "3";
+
+
+
+            string[] kurslar = new string[] {"1", "2", "3" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+            Console.WriteLine("Sayfa Sonu");
+        }
+    }
+}
